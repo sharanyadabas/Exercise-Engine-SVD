8d5432f8f4e773db9ae74091707cdbe46da914da
Sharanya Dabas <sd699@cornell.edu>
2024-03-27 15:42:56 -0400

    Removed exercises with empty descriptions

--- backend/scripts/converter.py
+++ backend/scripts/converter.py
@@ -1,13 +1,17 @@
 import json
 
-with open("../backend/init.json", "r", encoding="utf-8") as openfile:
+with open("complete_exercises.json", "r", encoding="utf-8") as openfile:
     data = json.load(openfile)
 openfile.close()
 
-for exercise in data["exercises"]:
-    exercise["Rating"] = str(float(exercise["Rating"]))
+new_exercise = []
+for exercise in data:
+    if not exercise["description"].strip():
+        continue
+    new_exercise.append(exercise)
 
-json_object = json.dumps(data, indent=2)
+
+json_object = json.dumps(new_exercise, indent=2)
 
 with open("newDataset.json", "w") as outfile:
     outfile.write(json_object)
