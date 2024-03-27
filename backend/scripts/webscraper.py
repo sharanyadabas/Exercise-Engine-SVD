3ef999b8e57c73ef0da703e50d864d7975cb2f79
Sharanya Dabas <sd699@cornell.edu>
2024-03-26 22:55:41 -0400

    Added try except if element is not found

--- backend/scripts/webscraper.py
+++ backend/scripts/webscraper.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import json
 import time
 
@@ -20,112 +21,123 @@ try:
         driver.get(url)
         time.sleep(2)  # Wait for page to load
 
-        exercises = driver.find_elements(By.CSS_SELECTOR, ".exercise_list article")
-        exercise_urls = [
-            exercise.find_element(
-                By.CSS_SELECTOR, ".main_info h2.title a"
-            ).get_attribute("href")
-            for exercise in exercises
-        ]
+        try:
+            exercises = driver.find_elements(By.CSS_SELECTOR, ".exercise_list article")
+            exercise_urls = [
+                exercise.find_element(
+                    By.CSS_SELECTOR, ".main_info h2.title a"
+                ).get_attribute("href")
+                for exercise in exercises
+            ]
+        except NoSuchElementException:
+            print("Page not found")
+            continue
 
         for exercise_url in exercise_urls:
             driver.get(exercise_url)
             time.sleep(2)  # Wait for page to load
 
-            # Description
-            description_elements = driver.find_elements(
-                By.CSS_SELECTOR, ".wpb_wrapper p"
-            )
-            description = (
-                " ".join([p.text for p in description_elements])
-                if description_elements
-                else None
-            )
+            try:
+                # Description
+                description_elements = driver.find_elements(
+                    By.CSS_SELECTOR, ".wpb_wrapper p"
+                )
+                description = (
+                    " ".join([p.text for p in description_elements])
+                    if description_elements
+                    else None
+                )
 
-            # Page Text
-            body_element = driver.find_element(By.TAG_NAME, "body")
-            page_text = (
-                body_element.text.replace("\n", " ")
-                .replace("\r", " ")
-                .replace("\t", " ")
-                .replace("\u2019", "'")
-                if body_element
-                else None
-            )
+                # Page Text
+                body_element = driver.find_element(By.TAG_NAME, "body")
+                page_text = (
+                    body_element.text.replace("\n", " ")
+                    .replace("\r", " ")
+                    .replace("\t", " ")
+                    .replace("\u2019", "'")
+                    if body_element
+                    else None
+                )
 
-            # Title
-            title_element = driver.find_element(By.CSS_SELECTOR, "h1.page_title")
-            title = title_element.text if title_element else None
+                # Title
+                title_element = driver.find_element(By.CSS_SELECTOR, "h1.page_title")
+                title = title_element.text if title_element else None
 
-            # Instructions
-            ordered_lists = driver.find_elements(By.CSS_SELECTOR, "ol")
-            instruction_elements = (
-                ordered_lists[0].find_elements(By.TAG_NAME, "li")
-                if len(ordered_lists) > 0
-                else None
-            )
-            instructions = (
-                {
-                    index + 1: element.text
-                    for index, element in enumerate(instruction_elements)
+                # Instructions
+                ordered_lists = driver.find_elements(By.CSS_SELECTOR, "ol")
+                instruction_elements = (
+                    ordered_lists[0].find_elements(By.TAG_NAME, "li")
+                    if len(ordered_lists) > 0
+                    else None
+                )
+                instructions = (
+                    {
+                        index + 1: element.text
+                        for index, element in enumerate(instruction_elements)
+                    }
+                    if instruction_elements
+                    else None
+                )
+
+                # Primary Muscles
+                muscle_elements = driver.find_elements(By.CSS_SELECTOR, ".vc_label")
+                primary_muscles = (
+                    [muscle.text for muscle in muscle_elements]
+                    if muscle_elements
+                    else None
+                )
+                if primary_muscles:
+                    primary_muscles = [
+                        item.split(" - ")[1] if " - " in item else item
+                        for item in primary_muscles
+                    ]
+
+                # Equipment
+                ul_element = driver.find_element(
+                    By.CSS_SELECTOR, ".spec_group.equipments .group-content ul"
+                )
+                li_elements = (
+                    ul_element.find_elements(By.TAG_NAME, "li") if ul_element else None
+                )
+                equipment = (
+                    [li.find_element(By.TAG_NAME, "span").text for li in li_elements]
+                    if li_elements
+                    else None
+                )
+
+                # Images
+                image_elements = driver.find_elements(By.TAG_NAME, "img")
+                second_image_url = (
+                    image_elements[1].get_attribute("src")
+                    if len(image_elements) >= 2
+                    else None
+                )
+                third_image_url = (
+                    image_elements[2].get_attribute("src")
+                    if len(image_elements) >= 3
+                    else None
+                )
+
+                exercise_data = {
+                    "title": title,
+                    "description": description,
+                    "instructions": instructions,
+                    "muscle-group": primary_muscles,
+                    "equipment": equipment,
+                    "exercise_image_url": second_image_url,
+                    "muscle_image_url": third_image_url,
+                    "all-text": page_text,
                 }
-                if instruction_elements
-                else None
-            )
-
-            # Primary Muscles
-            muscle_elements = driver.find_elements(By.CSS_SELECTOR, ".vc_label")
-            primary_muscles = (
-                [muscle.text for muscle in muscle_elements] if muscle_elements else None
-            )
-            if primary_muscles:
-                primary_muscles = [
-                    item.split(" - ")[1] if " - " in item else item
-                    for item in primary_muscles
-                ]
-
-            # Equipment
-            ul_element = driver.find_element(
-                By.CSS_SELECTOR, ".spec_group.equipments .group-content ul"
-            )
-            li_elements = (
-                ul_element.find_elements(By.TAG_NAME, "li") if ul_element else None
-            )
-            equipment = (
-                [li.find_element(By.TAG_NAME, "span").text for li in li_elements]
-                if li_elements
-                else None
-            )
-
-            # Images
-            image_elements = driver.find_elements(By.TAG_NAME, "img")
-            second_image_url = (
-                image_elements[1].get_attribute("src")
-                if len(image_elements) >= 2
-                else None
-            )
-            third_image_url = (
-                image_elements[2].get_attribute("src")
-                if len(image_elements) >= 3
-                else None
-            )
-
-            exercise_data = {
-                "title": title,
-                "description": description,
-                "instructions": instructions,
-                "muscle-group": primary_muscles,
-                "equipment": equipment,
-                "exercise_image_url": second_image_url,
-                "muscle_image_url": third_image_url,
-                "all-text": page_text,
-            }
-            if description is not None:
-                complete_exercise_data.append(exercise_data)
-            all_exercise_data.append(exercise_data)
-            print(f"{title} ({description is not None}): {counter} / {total_exercises}")
-            counter += 1
-
+                if description is not None:
+                    complete_exercise_data.append(exercise_data)
+                all_exercise_data.append(exercise_data)
+                print(
+                    f"{title} ({description is not None}): {counter} / {total_exercises}"
+                )
+                counter += 1
+            except NoSuchElementException:
+                print("Exercise not found")
+                continue
 
 finally:
     driver.quit()
