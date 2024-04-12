edf3a62b07725ff489d9a01e7323207d9317ac36
Matthew McAuley <mattwilliam618@gmail.com>
2024-04-12 19:37:14 -0400

    Implement SVD and create larger dataset
    - Used scipy to create SVD based on combined dataset
    of ratings and non-ratings
    - Only returns exercises that have a rating

--- backend/svd.py
+++ backend/svd.py
@@ -21,44 +21,23 @@ with open(json_file_path, "r", encoding="utf-8") as file:
                  for x in datalist
                  if len(x['all-text'].split()) > 50]
 
+count = 0
+for exercise in documents:
+    if exercise[0].upper() == exercise[0]:
+        count += 1
+print(count)
 
 vectorizer = TfidfVectorizer(stop_words = 'english', max_df = .7, min_df = 75)
 td_matrix = vectorizer.fit_transform([x[1] for x in documents])
 
-
 docs_compressed, s, words_compressed = svds(td_matrix, k=15)
-words_compressed = words_compressed.transpose()
-
-
-word_to_index = vectorizer.vocabulary_
-index_to_word = {i:t for t,i in word_to_index.items()}
-
-
-words_compressed_normed = normalize(words_compressed, axis = 1)
-
-def closest_words(word_in, words_representation_in, k = 10):
-    if word_in not in word_to_index: return "Not in vocab."
-    sims = words_representation_in.dot(words_representation_in[word_to_index[word_in],:])
-    asort = np.argsort(-sims)[:k+1]
-    return [(index_to_word[i],sims[i]) for i in asort[1:]]
-
-td_matrix_np = td_matrix.transpose().toarray()
-td_matrix_np = normalize(td_matrix_np)
-
-# word = 'strength'
-# print("Using SVD:")
-# for w, sim in closest_words(word, words_compressed_normed):
-#   try:
-#     print("{}, {:.3f}".format(w, sim))
-#   except:
-#     print("word not found")
-# print()
 
 docs_compressed_normed = normalize(docs_compressed)
 
-def closest_projects(project_index_in, project_repr_in, k = 5):
+def closest_projects(project_index_in, project_repr_in, k = 10):
     sims = project_repr_in.dot(project_repr_in[project_index_in,:])
-    asort = np.argsort(-sims)[:k+1]
+    asort = np.argsort(-sims)
+    asort = asort[asort >= count][:k+1]
     return [(documents[i][0],sims[i]) for i in asort[1:]]
 
 for i in range(10):
@@ -68,7 +47,3 @@ for i in range(10):
     for title, score in closest_projects(i, docs_compressed_normed):
         print("{}:{:.3f}".format(title, score))
     print()
-    print("Not using SVD (using directly term-document matrix):")
-    for title, score in closest_projects(i, td_matrix_np):
-        print("{}:{:.3f}".format(title, score))
-    print("--------------------------------------------------------\n")
